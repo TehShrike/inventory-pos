2fd19c11ef66468358a187eb8bde653464d14142
Josh Duff <me@JoshDuff.com>
2016-03-03 21:13:26 -0600

    A pretty dece build/globbing script

--- client/index.js
+++ client/index.js
@@ -6,6 +6,8 @@ var dragAndDropFiles = require('ractive-drag-and-drop-files')
 var mannish = require('mannish')
 var stateRouterRedux = require('state-router-redux-ractive')
 
+var globbed = require('./globbed')
+
 require('events').EventEmitter.defaultMaxListeners = 20
 
 var socket = socketio(window.location.host)
@@ -26,33 +28,12 @@ var context = {
 	mediator: mannish()
 }
 
-require('./states/app/app.js')(context)
-require('./states/app/customer-search/customer-search.js')(context)
-require('./states/app/customer/customer.js')(context)
-require('./states/app/configuration/configuration.js')(context)
-require('./states/scanner/scanner.js')(context)
-require('./states/scanner/menu/menu.js')(context)
-require('./states/scanner/add-plant/add-plant.js')(context)
-require('./states/scanner/add-plant/add-plant-change.js')(context)
-require('./states/scanner/add-plant/add-plant-save.js')(context)
-require('./states/scanner/select-growth-phase/select-growth-phase.js')(context)
-require('./states/scanner/select-room/select-room.js')(context)
-require('./states/scanner/select-strain/select-strain.js')(context)
-
-var initializeEmitToServerService = require('./services/emit-to-server.js')
-var initializeGoToStateService = require('./services/go-to-state.js')
-var initializeInventoryTypeService = require('./services/inventory-type.js')
-var initializeDocumentManagerService = require('./services/document-manager.js')
-var barcodeScanHandler = require('./services/barcode-scan-handler.js')
+globbed.states.forEach(createState => createState(context))
 
 socket.on('connect', function() {
 	var userId = 666 // why not
 	socket.emit('authenticate', userId, function(err, user) {
-		initializeEmitToServerService(context)
-		initializeGoToStateService(context)
-		initializeInventoryTypeService(context)
-		initializeDocumentManagerService(context)
-		barcodeScanHandler(context)
+		globbed.services.forEach(service => service(context))
 
 		stateRouter.evaluateCurrentRoute('app.customer-search')
 	})
---
+++ compile-client.js
@@ -0,0 +1,82 @@
+const browserify = require('browserify')
+const watchify = require('watchify')
+const fs = require('fs')
+const glob = require('glob')
+const all = require('async-all')
+
+const dev = process.argv[2] === 'dev'
+
+function main(dev) {
+	const b = buildBrowserifyPipeline(dev)
+	function bundle() {
+		buildGlobbed(() => {
+			console.log('rebuilding', new Date())
+			b.bundle().pipe(fs.createWriteStream('static/build.js'));
+		})
+	}
+
+	b.on('update', bundle)
+
+	bundle()
+}
+main(dev)
+
+function buildBrowserifyPipeline(dev) {
+	const b = browserify({
+		entries: ['./client/index.js'],
+		cache: {},
+		packageCache: {},
+		debug: dev
+	}).transform('babelify', {
+		presets: 'es2015',
+		plugins: 'transform-object-rest-spread'
+	}).transform('stringify')
+
+	if (dev) {
+		b.plugin(watchify)
+	}
+
+	return b
+}
+
+function buildGlobbed(cb) {
+	all({
+		services: cb => globNonTest('services/*.js', cb),
+		states: cb => globNonTest('states/**/*.js', cb)
+	}, (err, globs) => {
+		const servicesArray = getFilesAsRequireArray(globs.services)
+		const statesArray = getFilesAsRequireArray(globs.states)
+
+		const output = `
+module.exports = {
+	services: ${servicesArray},
+	states: ${statesArray}
+}`
+		fs.writeFileSync('client/globbed.js', output + '\n')
+		cb()
+	})
+}
+
+function globNonTest(path, cb) {
+	glob(path, {
+		cwd: 'client'
+	}, (err, files) => {
+		if (err) {
+			throw err
+		}
+
+		cb(err, files.filter(file => !/-test.js$/.test(file)).sort(sortGlobbedIncludes))
+	})
+}
+
+function sortGlobbedIncludes(fileA, fileB) {
+	var chunksA = fileA.split('/')
+	var chunksB = fileB.split('/')
+	return chunksA.length - chunksB.length
+}
+
+function getFilesAsRequireArray(files) {
+	return '[\n' + files.map(file => {
+		return `		require('./${file}')`
+	}).join(',\n') + '\n\t]'
+}
