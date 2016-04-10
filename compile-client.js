5a8ee9b1c2f76146a94ef0380ab228e13a475826
Josh Duff <me@JoshDuff.com>
2016-04-09 20:21:26 -0500

    Fine, I'll just use a symlink

--- compile-client.js
+++ compile-client.js
@@ -87,18 +87,16 @@ function buildBrowserifyPipeline(dev) {
 function buildGlobbed(cb) {
 	all({
 		services: cb => globNonTest('services/*.js', cb),
-		states: cb => globNonTest('states/**/*.js', cb),
-		shared: cb => globNonTest('../shared/*.js', cb)
+		states: cb => globNonTest('states/**/*.js', cb)
 	}, (err, globs) => {
 		const servicesArray = getFilesAsRequireArray(globs.services)
 		const statesArray = getFilesAsRequireArray(globs.states)
-		const sharedObject = getFilesAsRequireObject(globs.shared)
+		// const sharedObject = getFilesAsRequireObject(globs.shared)
 
 		const output = `
 module.exports = {
 	services: ${servicesArray},
-	states: ${statesArray},
-	shared: ${sharedObject}
+	states: ${statesArray}
 }`
 		fs.writeFileSync('client/globbed.js', output + '\n')
 		cb()
@@ -129,12 +127,12 @@ function getFilesAsRequireArray(files) {
 	}).join(',\n') + '\n\t]'
 }
 
-function getFilesAsRequireObject(files) {
-	return '{\n' + files.map(file => {
-		const key = camelize(file.substring('../shared/'.length, file.length - '.js'.length))
-		return `		${key}: require('./${file}')`
-	}).join(',\n') + '\n\t}'
-}
+// function getFilesAsRequireObject(files) {
+// 	return '{\n' + files.map(file => {
+// 		const key = camelize(file.substring('../shared/'.length, file.length - '.js'.length))
+// 		return `		${key}: require('./${file}')`
+// 	}).join(',\n') + '\n\t}'
+// }
 
 function buildCss(done) {
 	glob('client/**/*.css', (err, files) => {
